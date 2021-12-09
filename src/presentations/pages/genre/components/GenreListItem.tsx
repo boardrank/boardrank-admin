97a63d41176c8553a3551a2e8fbc746cdb82d41d
fine <dlgmltjr0925@gmail.com>
2021-12-09 18:48:39 +0900

    feature: Genre List 드래그 앤 드롭 기능 구현

--- src/presentations/pages/genre/components/GenreListItem.tsx
+++ src/presentations/pages/genre/components/GenreListItem.tsx
@@ -1,39 +1,59 @@
+import {
+  DraggableProvidedDragHandleProps,
+  DraggableProvidedDraggableProps,
+} from 'react-beautiful-dnd';
+import styled, { CSSProperties } from 'styled-components';
+
 import CloseIcon from '@mui/icons-material/Close';
 import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
 import { IconButton } from '@mui/material';
 import { Genre as Item } from '../../../../../out/typescript/models/Genre';
-import styled from 'styled-components';
+import { forwardRef } from 'react';
 
 interface GenreListItemProps {
   item: Item;
+  style?: CSSProperties;
+  draggableProps: DraggableProvidedDraggableProps;
+  dragHandleProps?: DraggableProvidedDragHandleProps;
 }
 
-const GenreListItem = ({
-  item: { id, name, code, order },
-}: GenreListItemProps) => {
-  return (
-    <StyledWrapper>
-      <td className="move">
-        <IconButton aria-label="drag-indicator" sx={{ width: 35, height: 35 }}>
-          <DragIndicatorIcon />
-        </IconButton>
-      </td>
-      <td className="id">{id}</td>
-      <td>{name}</td>
-      <td>{code}</td>
-      <td>{order}</td>
-      <td className="remove">
-        <IconButton aria-label="remove" sx={{ width: 30, height: 30 }}>
-          <CloseIcon sx={{ color: '#FF2A37', width: 20, height: 20 }} />
-        </IconButton>
-      </td>
-    </StyledWrapper>
-  );
-};
+const GenreListItem = forwardRef<
+  HTMLTableRowElement | null,
+  GenreListItemProps
+>(
+  (
+    { item: { id, name, code, order }, style, draggableProps, dragHandleProps },
+    ref,
+  ) => {
+    return (
+      <StyledWrapper
+        ref={ref}
+        className="tr"
+        {...draggableProps}
+        style={{ ...style }}>
+        <div className="td move">
+          <IconButton
+            aria-label="drag-indicator"
+            sx={{ width: 35, height: 35 }}
+            {...dragHandleProps}>
+            <DragIndicatorIcon />
+          </IconButton>
+        </div>
+        <div className="td id">{id}</div>
+        <div className="td">{name}</div>
+        <div className="td">{code}</div>
+        <div className="td">{order}</div>
+        <div className="td remove">
+          <IconButton aria-label="remove" sx={{ width: 30, height: 30 }}>
+            <CloseIcon sx={{ color: '#FF2A37', width: 20, height: 20 }} />
+          </IconButton>
+        </div>
+      </StyledWrapper>
+    );
+  },
+);
 
-const StyledWrapper = styled.tr`
-  min-height: 100px;
-
+const StyledWrapper = styled.div`
   .move,
   .remove {
     width: 40px;
@@ -47,12 +67,9 @@ const StyledWrapper = styled.tr`
     &:hover {
       button {
         opacity: 1;
+        cursor: grab;
       }
     }
-  }
-
-  .id {
-    width: 100px;
   }
 `;
 
--- src/presentations/pages/genre/components/GenreListPage.tsx
+++ src/presentations/pages/genre/components/GenreListPage.tsx
@@ -1,38 +1,81 @@
+import {
+  DragDropContext,
+  Draggable,
+  DropResult,
+  Droppable,
+  ResponderProvided,
+} from 'react-beautiful-dnd';
+
 import GenreListItem from './GenreListItem';
 import { Paper } from '@mui/material';
-import Table from '../../../common/components/Table';
 import styled from 'styled-components';
 import { useGenreList } from '../hooks/useGenreList';
+
+const getItemStyle = (isDragging: boolean, draggableStyle: any) => {
+  return {
+    ...draggableStyle,
+  };
+};
 
 const GenreListPage = () => {
   const { genreList } = useGenreList();
 
+  const handleDragEnd = (result: DropResult, provided: ResponderProvided) => {
+    console.log(result);
+  };
+
   return (
-    <StyledWrapper>
-      <div className="table-container">
-        <Paper className="paper-wrapper">
-          <div className="table-wrapper">
-            <Table>
-              <thead>
-                <tr>
-                  <th></th>
-                  <th>id</th>
-                  <th>name</th>
-                  <th>code</th>
-                  <th>order</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                {genreList.genres.map(genre => (
-                  <GenreListItem key={genre.id} item={genre} />
-                ))}
-              </tbody>
-            </Table>
-          </div>
-        </Paper>
-      </div>
-    </StyledWrapper>
+    <DragDropContext onDragEnd={handleDragEnd}>
+      <StyledWrapper>
+        <div className="table-container">
+          <Paper className="paper-wrapper">
+            <div className="table-wrapper">
+              <div className="table">
+                <div className="thead">
+                  <div className="tr">
+                    <div className="th"></div>
+                    <div className="th">id</div>
+                    <div className="th">name</div>
+                    <div className="th">code</div>
+                    <div className="th">order</div>
+                    <div className="th"></div>
+                  </div>
+                </div>
+                <Droppable droppableId="droppable">
+                  {(provided, snapshot) => (
+                    <div
+                      className="tbody"
+                      {...provided.droppableProps}
+                      ref={provided.innerRef}>
+                      {genreList.genres.map((genre, index) => (
+                        <Draggable
+                          key={genre.id}
+                          draggableId={`${genre.id}`}
+                          index={index}>
+                          {(provided, snapshot) => (
+                            <GenreListItem
+                              item={genre}
+                              ref={provided.innerRef}
+                              draggableProps={provided.draggableProps}
+                              dragHandleProps={provided.dragHandleProps}
+                              style={getItemStyle(
+                                snapshot.isDragging,
+                                provided.draggableProps.style,
+                              )}
+                            />
+                          )}
+                        </Draggable>
+                      ))}
+                      {provided.placeholder}
+                    </div>
+                  )}
+                </Droppable>
+              </div>
+            </div>
+          </Paper>
+        </div>
+      </StyledWrapper>
+    </DragDropContext>
   );
 };
 
@@ -63,7 +106,6 @@ const StyledWrapper = styled.div`
 
       &::-webkit-scrollbar,
       &::-webkit-scrollbar-thumb {
-        overflow: visible;
         border-radius: 2px;
       }
 
@@ -71,8 +113,81 @@ const StyledWrapper = styled.div`
         background: rgba(0, 0, 0, 0.2);
       }
 
-      table {
+      .table {
+        width: 100%;
         height: 100%;
+        background-color: transparent;
+        margin: 0;
+
+        .tr {
+          width: 100%;
+          display: flex;
+          flex-direction: row;
+          padding: 0 5px;
+        }
+
+        .thead {
+          color: #7b809a;
+          font-size: 0.7em;
+          font-weight: bold;
+          position: sticky;
+          top: 0;
+          background-color: white;
+
+          .tr {
+            text-transform: uppercase;
+
+            .th {
+              text-align: center;
+              padding: 15px 15px 10px;
+              background-color: white;
+              width: 40px;
+
+              &:nth-child(2) {
+                width: 100px;
+              }
+
+              &:nth-child(n + 3):nth-child(-n + 5) {
+                flex: 1;
+              }
+            }
+          }
+        }
+
+        .tbody {
+          font-size: 0.875em;
+          border-top: none;
+
+          .tr {
+            border-top: 1px solid #eee;
+            cursor: pointer;
+
+            &:hover {
+              background-color: #eee;
+            }
+
+            &:active {
+              background-color: #ddd;
+            }
+
+            .td {
+              display: flex;
+              text-align: center;
+              justify-content: center;
+              align-items: center;
+              width: 40px;
+              padding: 10px 0;
+
+              &:nth-child(2) {
+                width: 100px;
+              }
+
+              &:nth-child(n + 3):nth-child(-n + 5) {
+                flex: 1;
+              }
+            }
+          }
+        }
       }
     }
   }
