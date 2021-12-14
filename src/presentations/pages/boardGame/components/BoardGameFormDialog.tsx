4a39acd271938dbff562c95a1a1c8a2a1cfaff0f
fine <dlgmltjr0925@gmail.com>
2021-12-15 00:46:44 +0900

    feature: Image Crop 기능 구현

--- src/presentations/pages/boardGame/components/BoardGameFormDialog.tsx
+++ src/presentations/pages/boardGame/components/BoardGameFormDialog.tsx
@@ -169,7 +169,8 @@ const BoardGameFormDialog = ({
             <Button
               className="btn-withdrawal"
               variant="contained"
-              onClick={handleSubmitDelete}>
+              onClick={handleSubmitDelete}
+            >
               제 거
             </Button>
           )}
