d1b0920d527cf6b6bddeac3976b4f68249c5d3ee
fine <dlgmltjr0925@gmail.com>
2022-01-05 15:08:50 +0900

    feature: 불필요한 사용자 정보 조회 제거

--- src/repositories/recoil/userState.recoil.ts
+++ src/repositories/recoil/userState.recoil.ts
@@ -2,6 +2,7 @@ import { User } from '../../../out/typescript/models/User';
 import { getUser } from '../api/user.repository';
 import { selector } from 'recoil';
 import { UserRequestIdState } from './userRequestIdState.recoil';
+import { getAccessToken } from '../localStorage/auth.repository';
 
 const UserStateKey = 'UserState';
 
@@ -9,6 +10,7 @@ export const userState = selector<User | null>({
   key: UserStateKey,
   get: async ({ get }) => {
     get(UserRequestIdState);
+    if (!getAccessToken()) return null;
     try {
       const res = await getUser();
       return res.data.user;
